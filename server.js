bca4ffbf9e2d8d940e1636f4dd4c70300ad26480
王雪磊 <xuelei.wang@lingxi.co>
2018-12-18 19:20:02 +0800

    fix

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
  * Created by wangxuelei on 2018/12/11.
  */
 'use strict';
+const http = require('http');
+const connectRedis = require('./util/connectRedis');
+
+connectRedis({ create: true });
+
 
 const app = require('./app');
-const connectRedis = require('util/connectRedis');
-connectRedis({create:true})
+
+const server = http.createServer(app);
 
 
-const server = require('http').createServer(app);
-
-const PORT_ADMIN = process.env.PORT_ADMIN || process.env.PORT || 4010;
+const PORT_ADMIN = 4010;
 server.listen(PORT_ADMIN);
 
 console.log(`Admin server start on ${PORT_ADMIN}`);
